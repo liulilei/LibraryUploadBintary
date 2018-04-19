9e40eeeb8d433ccbdf28a094ec77b83e859a8cf0
liulei <CHX123OK>
2018-04-19 17:36:45 +0800

    Initial commit

---
+++ libraryuploadbintrary/src/main/java/test/library/libraryuploadbintrary/ToastUtils.java
@@ -0,0 +1,54 @@
+package test.library.libraryuploadbintrary;
+
+import android.content.Context;
+import android.os.Looper;
+import android.widget.Toast;
+
+/**
+ * ToastUtils
+ */
+public class ToastUtils {
+
+    public static void show(Context context, int resId) {
+        show(context, context.getResources().getText(resId), Toast.LENGTH_SHORT);
+    }
+
+    public static void show(Context context, int resId, int duration) {
+        show(context, context.getResources().getText(resId), duration);
+    }
+
+    public static void show(Context context, CharSequence text) {
+        show(context, text, Toast.LENGTH_SHORT);
+    }
+
+    public static void show(Context context, int resId, Object... args) {
+        show(context, String.format(context.getResources().getString(resId), args), Toast.LENGTH_SHORT);
+    }
+
+    public static void show(Context context, String format, Object... args) {
+        show(context, String.format(format, args), Toast.LENGTH_SHORT);
+    }
+
+    public static void show(Context context, int resId, int duration,
+                            Object... args) {
+        show(context, String.format(context.getResources().getString(resId), args), duration);
+    }
+
+    public static void show(Context context, String format, int duration, Object... args) {
+        show(context, String.format(format, args), duration);
+    }
+
+    public static void show(Context context, CharSequence text, int duration) {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            makeToast(context, text, duration);
+        } else {
+            Looper.prepare();
+            makeToast(context, text, duration);
+            Looper.loop();
+        }
+    }
+
+    public static void makeToast(Context context, CharSequence msg, int duration) {
+        Toast.makeText(context, msg, duration).show();
+    }
+}
